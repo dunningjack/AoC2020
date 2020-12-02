ca81d8eb52a5af2def686d672ba46ae7265c7250
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-12-02 11:03:10 +0000

    Day 2 work before lecture

--- Day 2/Day 2 Part 1.py
+++ Day 2/Day 2 Part 1.py
@@ -1,15 +1,29 @@
-file = passwords
+file = "passwords"
+pword = []
+reqs = []
+NumberOfInstances = []
+LetterRequired = []
+LowerLimit = []
+UpperLimit = []
 
 with open(file, 'r') as f:
 
+    for line in f:
+        pword.append((line.rsplit(":")))
+        reqs.append((line.lsplit(":")))
 
-for line in lines:
-    pword.append((line.rsplit(":")))
-    reqs.append((line.lsplit(":")))
+    for req in reqs:
+        for i in range(0, len(req)):
+            NumberOfInstances.append(req.lsplit(" "))
+            LetterRequired.append(req.rsplit(" "))
 
-for req in reqs:
-    for i in range(0, len(req)):
-        NumberOfInstances = req.lsplit(" ")
-        LetterRequired = req.rsplit(" ")
+    for j in range(0, len(NumberOfInstances)):
+        LowerLimit.append(NumberOfInstances[j].lsplit("-"))
+        UpperLimit.append(NumberOfInstances[j].rsplit("-"))
 
-for j in range(0, len(NumberOfInstances)):
+    for k in range(0,len(pword)):
+        if pword[k].count(LetterRequired[k]) == NumberOfInstances[k]:
+            print(pword)
+            break
+        else:
+            continue
