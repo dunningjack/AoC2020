bbd0f5921c6c2294c01035bf667d209da603358d
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-12-02 08:38:46 +0000

    Day 2 work before lecture

---
+++ Day 2/Day 2 Part 1.py
@@ -0,0 +1,15 @@
+file = \input
+
+with open(file, 'r') as f:
+
+
+for line in lines:
+    pword.append((line.rsplit(":")))
+    reqs.append((line.lsplit(":")))
+
+for req in reqs:
+    for i in range(0, len(req)):
+        NumberOfInstances = req.lsplit(" ")
+        LetterRequired = req.rsplit(" ")
+
+for j in range(0, len(NumberOfInstances)):
