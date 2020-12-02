7ce9a65737c8ce1cd2cd85ca96fded0fd3c72c9f
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-12-02 11:46:58 +0000

    Day 2

--- Day 2/Day 2 Part 1.py
+++ Day 2/Day 2 Part 1.py
@@ -10,19 +10,19 @@ with open(file, 'r') as f:
 
     for line in f:
         pword.append((line.rsplit(":")))
-        reqs.append((line.lsplit(":")))
+        reqs.append((line.split(":", 1)))
 
     for req in reqs:
         for i in range(0, len(req)):
-            NumberOfInstances.append(req.lsplit(" "))
-            LetterRequired.append(req.rsplit(" "))
+            NumberOfInstances.append(req[i].split(" ", 1))
+            LetterRequired.append(req[i].rsplit(" "))
 
     for j in range(0, len(NumberOfInstances)):
-        LowerLimit.append(NumberOfInstances[j].lsplit("-"))
-        UpperLimit.append(NumberOfInstances[j].rsplit("-"))
+        LowerLimit.append((NumberOfInstances[j].split("-", 1)))
+        UpperLimit.append((NumberOfInstances[j].rsplit("-")))
 
-    for k in range(0,len(pword)):
-        if pword[k].count(LetterRequired[k]) == NumberOfInstances[k]:
+    for k in range(0, len(pword)):
+        if pword[k].count((LetterRequired[k])) == NumberOfInstances[k]:
             print(pword)
             break
         else:
