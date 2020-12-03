39a0d18d79493685a317d322aa85038c6409f37e
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-12-03 02:00:01 +0000

    Day 2 Rewrite 2: Part 1 Complete, part 2 in progress. Will complete before 5am

--- Day 2/Day 2 Part 2.py
+++ Day 2/Day 2 Part 2.py
@@ -1,21 +1,27 @@
 file = "passwords"
 total = 0
+valid = 0
 
 with open(file, 'r') as f:
 
     for line in f:
         parts = line.split(" ")
         bounds = parts[0].split("-")
-        LB = bounds[0]
-        UB = bounds[1]
+        LB = int(bounds[0])
+        UB = int(bounds[1])
         search = parts[1].strip(":")
 
 
         # for j in range(0, len(parts)):
             # if parts[2][j] == search:
                 # count += 1
-        count = parts[2].count(search)
+        pword = parts[2]
+        count = pword.count(search)
 
         if count >= int(LB) and count <= int(UB):
             total += 1
-        print(total)
+
+        if (pword[LB] == search) is not (pword[UB] == search):
+            valid += 1
+
+print(valid)
