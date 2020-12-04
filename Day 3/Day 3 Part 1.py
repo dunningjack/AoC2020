46af952b2905eb2033f0d3b27e0506b0f0ebe00b
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-12-04 14:54:47 +0000

    Day 4

--- Day 3/Day 3 Part 1.py
+++ Day 3/Day 3 Part 1.py
@@ -1,21 +1,18 @@
 linesize = 0
-trees = 0
+treecount = 0
+x = 0
+y = 2
 
 with open("Map", "r") as f:
     lines = f.readlines()
     # reads all lines into a list of strings
 
-
-    for LineNumber in range(0, len(f.readlines())):
-        while LineNumber <= len(f.readlines()):
-            for j in range(0, 2):
-                if lines[LineNumber][j] == "#":
-                    # if there is a hash in LineNumber position j, then tree count needs to be updated
-                    trees += 1
-                    # if character is a hash, then add one to the count of trees and continue
-                    LineNumber += 1
-                    # proceed to next line of the file but need to make position starting position [j] again to /
-                    # to continue the count
-                    break
+    for i in range(0, len(lines)):
+        for j in range(0, len(lines[i])):
+            if j % 3 == 1:
+                if lines[i][j] == "#":
+                    treecount += 1
+            j = 0
 
 
+print(treecount)
